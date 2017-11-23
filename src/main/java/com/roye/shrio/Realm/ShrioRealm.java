7a07b4c79a856439864631f0840a1d6d8ec58754
royeyu <1755431269@qq.com>
2017-11-23 21:45:33 +0800

    1. 加入shrio

---
+++ src/main/java/com/roye/shrio/Realm/ShrioRealm.java
@@ -0,0 +1,20 @@
+package com.roye.shrio.Realm;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.realm.Realm;
+
+public class ShrioRealm implements Realm {
+    public String getName() {
+        return null;
+    }
+
+    public boolean supports(AuthenticationToken authenticationToken) {
+        return false;
+    }
+
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        return null;
+    }
+}
